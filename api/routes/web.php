0ba3950f8357ce854f42eab086f8f1ebc40f1040
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-03-03 21:56:56 +0500

    chore: diagnostic routes follow-up

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test-route', function () {
-    return response()->json(['message' => 'Web route is working']);
+    return response()->json(['message' => 'Web route is working (no prefix)']);
+});
+
+Route::get('/api/test-route', function () {
+    return response()->json(['message' => 'Web route is working (with api prefix)']);
 });
 
 Route::get('/', function () {
