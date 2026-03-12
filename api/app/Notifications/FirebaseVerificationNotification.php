79ed041230bbb1b6c8a5c387d6ccb435796195f8
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-03-12 17:30:36 +0500

    feat: use SaaS dynamic template for Firebase verification emails

--- api/app/Notifications/FirebaseVerificationNotification.php
+++ api/app/Notifications/FirebaseVerificationNotification.php
@@ -4,40 +4,25 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
-use App\Models\Setting;
+use App\Notifications\BaseDynamicNotification;
 
-class FirebaseVerificationNotification extends Notification implements ShouldQueue
+class FirebaseVerificationNotification extends BaseDynamicNotification implements ShouldQueue
 {
     use Queueable;
 
-    protected $verificationUrl;
-    protected $userName;
-
+    /**
+     * Create a new notification instance.
+     * 
+     * @param string $verificationUrl The Firebase verification link
+     * @param string $userName The name of the user
+     */
     public function __construct($verificationUrl, $userName)
     {
-        $this->verificationUrl = $verificationUrl;
-        $this->userName = $userName;
-    }
-
-    public function via($notifiable)
-    {
-        return ['mail'];
-    }
-
-    public function toMail($notifiable)
-    {
-        $appName = Setting::getValue('app_name', 'UpgradedProxy');
-        
-        return (new MailMessage)
-            ->subject('Verify Your Email - ' . $appName)
-            ->greeting('Hello ' . $this->userName . '!')
-            ->line('Thank you for signing up for ' . $appName . '.')
-            ->line('Please click the button below to verify your email address and activate your account.')
-            ->action('Verify Email', $this->verificationUrl)
-            ->line('This link will take you to Firebase to confirm your verification.')
-            ->line('After verifying, please return to the app to access your dashboard.')
-            ->line('If you did not create an account, no further action is required.');
+        parent::__construct('email_verification', [
+            'verification_url' => $verificationUrl,
+            'user' => [
+                'name' => $userName
+            ]
+        ]);
     }
 }
